5aef2dcf8aa2d12b488008d6d1140869b5a915c6
Vlad <koshmakvl@gmail.com>
2021-01-15 21:31:21 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const showRequest = () =>{
+    console.log('CURRENCY',CURRENCY);
+    console.log('BASKET',BASKET);
+    console.log('TOP_MENU',TOP_MENU);
+    console.log('MENU',MENU);
+    console.log('NEWS',NEWS);
+    console.log('BANNER',BANNER);
+    console.log('PROMOTIONS', PROMOTIONS);
+    console.log('BUYING_RIGHT_NOW',BUYING_RIGHT_NOW);
+}
+
+showRequest();
